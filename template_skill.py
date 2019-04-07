63789259fc3c1327bf9d6bea37c2e4c282fa972b
Jean-Michel Lekston <lekston.jm@gmail.com>
2019-04-07 12:41:51 +0200

    Update template_skill.py

--- template_skill.py
+++ template_skill.py
@@ -2,20 +2,20 @@ from pytlas import training, translations, intent, meta
 
 @training('en')
 def en_training(): return """
-%[TEMPLATE_LAUCH_SKILL]
+%[TEMPLATE_LAUNCH_SKILL]
   LAUNCH TEMPLATE SKILL
 """
 
 @training('fr')
 def fr_training(): return """
-%[TEMPLATE_LAUCH_SKILL]
+%[TEMPLATE_LAUNCH_SKILL]
   LANCE LA COMPETENCE TEMPLATE  
 """
 
 @meta()
 def help_meta(_): return {
   'name': _('TEMPLATE_SKILL_NAME'),
-  'description': _('TEMPLATE_SKILL_SHORT_DSCRIPTION'),
+  'description': _('TEMPLATE_SKILL_SHORT_DESCRIPTION'),
   'author': 'TEMPLATE_AUTHOR_NAME',
   'version': 'TEMPLATE_VERSION',
   'homepage': 'TEMPLATE_SKILL_GIT_URL',
@@ -26,7 +26,7 @@ def fr_translations(): return {
   'TEMPLATE_ANSWER': 'TEMPLATE_REPONSE',
 }
 
-@intent('TEMPLATE_LAUCH_SKILL')
-def on_TEMPLATE_LAUCH_SKILL(req):
+@intent('TEMPLATE_LAUNCH_SKILL')
+def on_TEMPLATE_LAUNCH_SKILL(req):
   req.agent.answer(req._('TEMPLATE_ANSWER'))
   return req.agent.done()
