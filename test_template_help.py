3e16e3daffa7a72854ed6b003c05d8c0469d28e2
lekstonjm <lekston.jm@gmail.com>
2019-04-07 12:46:22 +0200

    fixe typos ^

--- test_template_help.py
+++ test_template_help.py
@@ -4,10 +4,8 @@ import os
 
 class Test_TEMPLATE_SKILL:
 
-  def test_TEMPLATE_LAUCH_SKILL(self):
+  def test_TEMPLATE_LAUNCH_SKILL(self):
     agent = create_skill_agent(os.path.dirname(__file__), lang='en')
-    agent.parse('LAUCH TEMPLATE SKILL')
+    agent.parse('LAUNCH TEMPLATE SKILL')
     call = agent.model.on_answer.get_call()
     expect(call.text).to.equal('TEMPLATE_ANSWER')
-
-
